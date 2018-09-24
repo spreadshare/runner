c514dd20676f6e74d009bb91d1068b91b9691b58
HugoPeters1024 <hpeters1024@gmail.com>
2018-09-24 12:52:56 +0200

    Remove legacy strategy implementation

--- SpreadShare/Strategy/Implementations/SimpleBandWagonStrategy.cs
+++ SpreadShare/Strategy/Implementations/SimpleBandWagonStrategy.cs
@@ -22,19 +22,7 @@ namespace SpreadShare.Strategy.Implementations
         {
             protected override void ValidateContext()
             {
-                Logger.LogInformation("Opening the entry state...");
-                Logger.LogInformation("Placing buy order...");
-                try
-                {
-                    long orderId = TradingService.PlaceMarketOrder("ETHBNB", OrderSide.Buy, 1);
-                    Context.SetObject("orderId", orderId);
-                }
-                catch(Exception e)
-                {
-                    Logger.LogCritical("Buy order failed, exiting.");
-                    Logger.LogCritical(e.Message);
-                    throw;
-                }
+                Logger.LogInformation("Welcome to the entry state, I do nothing yet.");
             }
 
             public override ResponseObject OnCandle(Candle c)
