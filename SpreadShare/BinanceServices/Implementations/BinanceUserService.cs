2b57fc447e516b1577c6a6e6e39a9479cbea1055
Yorick van Zweeden <git@yorickvanzweeden.nl>
2018-10-03 12:34:02 +0200

    Add ListenKeyManager with autorenewal

--- SpreadShare/BinanceServices/Implementations/BinanceUserService.cs
+++ SpreadShare/BinanceServices/Implementations/BinanceUserService.cs
@@ -1,7 +1,11 @@
-﻿using Binance.Net;
+﻿using System;
+using Binance.Net;
+using Binance.Net.Objects;
+using CryptoExchange.Net.Logging;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using SpreadShare.Models;
+using SpreadShare.Strategy;
 
 namespace SpreadShare.BinanceServices.Implementations
 {
@@ -9,13 +13,16 @@ namespace SpreadShare.BinanceServices.Implementations
     {
         private BinanceClient _client;
         private BinanceSocketClient _socketclient;
+        private ListenKeyManager _listenKeyManager;
+
         private readonly IConfiguration _configuration;
         private readonly ILogger _logger;
-
+        private readonly ILoggerFactory _loggerFactory;
 
         public BinanceUserService(ILoggerFactory loggerFactory, IConfiguration configuration)
         {
-            _logger = loggerFactory.CreateLogger(GetType());
+            _loggerFactory = loggerFactory;
+            _logger = _loggerFactory.CreateLogger(GetType());
             _configuration = configuration;
         }
 
@@ -26,35 +33,61 @@ namespace SpreadShare.BinanceServices.Implementations
         {
             //Setup the clients
             _client = new BinanceClient();
-            _socketclient = new BinanceSocketClient();
+            var options = new BinanceSocketClientOptions { LogVerbosity = LogVerbosity.Debug };
+            _socketclient = new BinanceSocketClient(options);
+
+            // Set credentials
             string apikey = _configuration.GetValue<string>("BinanceCredentials:api-key");
             string apisecret = _configuration.GetValue<string>("BinanceCredentials:api-secret");
             _client.SetApiCredentials(apikey, apisecret);
-            _socketclient.SetApiCredentials(apikey, apisecret);
 
-            string listenKey;
-            var getListenKey = _client.StartUserStream();
-            if (getListenKey.Success)
-                listenKey = getListenKey.Data.ListenKey;
-            else
+            // Get listen key
+            _listenKeyManager = new ListenKeyManager(_loggerFactory, _client);
+
+            // Setup streams
+            return EnableStreams();
+        }
+
+
+        /// <summary>
+        /// Enable streams for 24 hours
+        /// </summary>
+        /// <returns>If this operation succeeded</returns>
+        private ResponseObject EnableStreams()
+        {
+            _logger.LogInformation($"Enabling streams at {DateTime.UtcNow}");
+
+            // Obtain listenKey
+            var response = _listenKeyManager.Obtain();
+            if (!response.Success)
             {
-                _logger.LogCritical($"Unable to obtain Listen Key for binance websocket: {getListenKey.Error.Message}");
+                _logger.LogError("Unable to obtain listenKey");
                 return new ResponseObject(ResponseCodes.Error);
             }
-
+            var listenKey = response.Data;
 
             //Start socket connection
-            var succesOrderBook = _socketclient.SubscribeToUserStream(listenKey,
-                (accountInfoUpdate) =>
+            var succesOrderBook = _socketclient.SubscribeToUserStream(
+                listenKey,
+                accountInfoUpdate =>
                 {
-
+                    // Not implemented
                 },
-                (orderInfoUpdate) =>
-                {
-                    OnOrderUpdate(orderInfoUpdate);
-                });
-            _logger.LogInformation("Binance User Service was succesfully started!");
+                OnOrderUpdate);
 
+            // Set error handlers
+            succesOrderBook.Data.Closed += () =>
+            {
+                _logger.LogCritical($"Connection got closed at {DateTime.UtcNow}. Attempt to open socket");
+                EnableStreams();
+            };
+            succesOrderBook.Data.Error += e =>
+            {
+                _logger.LogError($"Connection got error at {DateTime.UtcNow}: {e}");
+                EnableStreams();
+            };
+
+            _logger.LogInformation("Binance User Service was successfully started!");
             return new ResponseObject(ResponseCodes.Success);
         }
 
---
+++ SpreadShare/BinanceServices/Implementations/ListenKeyManager.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Threading;
+using Binance.Net;
+using Microsoft.Extensions.Logging;
+using SpreadShare.Models;
+
+namespace SpreadShare.BinanceServices.Implementations
+{
+    internal class ListenKeyManager
+    {
+        private readonly BinanceClient _client;
+        private readonly ILogger _logger;
+        private readonly int _interval;
+
+        private string _listenKey;
+        private Timer _timer;
+        private bool _isRetry;
+
+
+        /// <summary>
+        /// Constructor: Create logger and set BinanceClient
+        /// </summary>
+        /// <param name="loggerFactory">Creates logger</param>
+        /// <param name="client">Creates BinanceClient</param>
+        /// <param name="interval">Renewal interval (default: 30min)</param>
+        public ListenKeyManager(ILoggerFactory loggerFactory, BinanceClient client, int interval = 30 * 60 * 1000)
+        {
+            _logger = loggerFactory.CreateLogger("ListenKeyManager");
+            _client = client;
+            _interval = interval;
+        }
+
+        /// <summary>
+        /// Obtain listenkey that is valid for 24 hours (auto renews every 30 min)
+        /// </summary>
+        /// <returns>ListenKey valid for 24 hours</returns>
+        public ResponseObject<string> Obtain()
+        {
+            // Cleanup previous instance
+            Cleanup();
+
+            // Get listen key
+            var getListenKey = _client.StartUserStream();
+            if (!getListenKey.Success)
+            {
+                _logger.LogCritical($"Unable to obtain ListenKey for Binance WebSocket: {getListenKey.Error.Message}");
+                return new ResponseObject(ResponseCodes.Error);
+            }
+            _listenKey = getListenKey.Data.ListenKey;
+
+            // Set timer every 30 min for autorenewal
+            SetTimer();
+
+            return new ResponseObject<string>(ResponseCodes.Success, _listenKey);
+        }
+
+        /// <summary>
+        /// Clears timer and listenkey
+        /// </summary>
+        private void Cleanup()
+        {
+            // Clear timer
+            _timer.Dispose();
+
+            // Clear listenKey
+            _listenKey = null;
+
+            // Clear retry
+            _isRetry = false;
+        }
+
+        /// <summary>
+        /// Set timer for every 30 minutes autorenewal
+        /// </summary>
+        private void SetTimer()
+        {
+            var autoEvent = new AutoResetEvent(true);
+
+            // Set timer to start in 30 min, every 30 min
+            _timer = new Timer(Renew, autoEvent, _interval, _interval);
+        }
+
+        /// <summary>
+        /// Renew listenkey
+        /// </summary>
+        /// <param name="stateInfo"></param>
+        private void Renew(object stateInfo)
+        {
+            _logger.LogInformation($"Requesting renewal of listenKey {_listenKey} at {DateTime.UtcNow}");
+            var renewal = _client.KeepAliveUserStream(_listenKey);
+
+            // If renewal error'ed
+            if (!renewal.Success)
+            {
+                _logger.LogError($"Could not renew listenKey {_listenKey} at {DateTime.UtcNow}");
+                _logger.LogError($"Error {renewal.Error.Code}: {renewal.Error.Message}");
+
+                if (!_isRetry)
+                {
+                    _isRetry = true;
+                    Renew(stateInfo);
+                }
+                else
+                {
+                    _logger.LogCritical($"ListenKey could not be obtained after a retry at {DateTime.UtcNow}. Expect a disconnect");
+                    /* ListenKey will expire in ~60 minutes. It's very likely that the stream has
+                     * reached it's 24 hour expiration mark. If the socket is closed, UserService
+                     * will request a new key.
+                     */
+                }
+            }
+
+            // Renewal succeeded
+            _logger.LogInformation($"Renewed listenKey {_listenKey} at {DateTime.UtcNow}");
+        }
+    }
+}
