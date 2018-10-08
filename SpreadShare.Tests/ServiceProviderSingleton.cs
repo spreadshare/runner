c075523d16b69bbe0a53352eb3cb3e7dcfd19d66
Yorick van Zweeden <git@yorickvanzweeden.nl>
2018-10-08 02:32:21 +0200

    Replace unit-testing solution with SpreadShare.Tests project

---
+++ SpreadShare.Tests/ServiceProviderSingleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SpreadShare;
+
+namespace Tests
+{
+    internal class ServiceProviderSingleton
+    {
+        private static readonly Lazy<ServiceProviderSingleton> lazy =
+            new Lazy<ServiceProviderSingleton>(() => new ServiceProviderSingleton());
+
+        public static ServiceProviderSingleton Instance => lazy.Value;
+        public IServiceProvider ServiceProvider;
+
+        private ServiceProviderSingleton()
+        {
+            // Create service collection
+            IServiceCollection services = new ServiceCollection();
+
+            // Configure services - Provide depencies for services
+            Startup startup = new Startup();
+            startup.ConfigureServices(services);
+            Startup.ConfigureBusinessServices(services);
+
+            // Create service provider
+            ServiceProvider = services.BuildServiceProvider();
+
+            // Configure application
+            Startup.Configure(ServiceProvider, (ILoggerFactory)ServiceProvider.GetService(typeof(ILoggerFactory)));
+        }
+    }
+}
---
+++ SpreadShare.Tests/UserService/ListenKeyManagerTests.cs
@@ -0,0 +1,125 @@
+﻿using System.Linq;
+using System.Threading;
+using Binance.Net;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using SpreadShare.BinanceServices.Implementations;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Tests
+{
+    public class ListenKeyManagerTests : BaseTest
+    {
+        public ListenKeyManagerTests(ITestOutputHelper outputHelper) : base(outputHelper)
+        {
+        }
+
+        /// <summary>
+        /// Tests if a ListenKey can be obtained
+        /// Assumption: Correct API Credentials
+        /// </summary>
+        [Fact]
+        public void ListenKeyObtainTest()
+        {
+            var listenKeyManager = Setup(10000);
+
+            // Obtain listenKey
+            var response = listenKeyManager.Obtain();
+            if (!response.Success)
+            {
+                Logger.LogError("Unable to obtain listenKey");
+                Assert.True(false);
+            }
+            var listenKey = response.Data;
+            Logger.LogInformation($"ListenKey obtained: {listenKey}");
+            Assert.True(listenKey != null & listenKey.Length > 10);
+        }
+
+        /// <summary>
+        /// Validates that an error response is given when the API
+        /// Credentials are wrong
+        /// </summary>
+        [Fact]
+        public void ListenKeyInvalidCredentialsTest()
+        {
+            var listenKeyManager = SetupInvalidCredentials(10000);
+
+            // Obtain listenKey
+            var response = listenKeyManager.Obtain();
+            if (response.Success)
+            {
+                Logger.LogError("I should not be able to obtain a listenKey");
+                Assert.True(false);
+            }
+        }
+
+        /// <summary>
+        /// Tests whether the listenkey is automatically renewed
+        /// </summary>
+        [Fact]
+        public void ListenKeyRenewalTest()
+        {
+            var listenKeyManager = Setup(3000);
+
+            // Obtain listenKey
+            var response = listenKeyManager.Obtain();
+            if (!response.Success)
+            {
+                Logger.LogError("Unable to obtain listenKey");
+                return;
+            }
+
+            // Sleep 10 seconds to autorenew three times
+            Thread.Sleep(10000);
+            var j = TestLoggingProvider.Messages.Count(message => message.Contains("Renewed listenKey"));
+            if (j <= 2)
+            {
+                Assert.True(false);
+            }
+        }
+
+        /// <summary>
+        /// Create a valid ListenKeyManager
+        /// </summary>
+        /// <param name="interval">Autorenewal interval</param>
+        /// <returns>Valid ListenKeyManager</returns>
+        private ListenKeyManager Setup(int interval)
+        {
+            var serviceProvider = ServiceProviderSingleton.Instance.ServiceProvider;
+            var configuration = (IConfiguration)serviceProvider.GetService(typeof(IConfiguration));
+            var loggerFactory = (ILoggerFactory)serviceProvider.GetService(typeof(ILoggerFactory));
+
+            //Setup the clients
+            var client = new BinanceClient();
+
+            // Set credentials
+            string apikey = configuration.GetValue<string>("BinanceCredentials:api-key");
+            string apisecret = configuration.GetValue<string>("BinanceCredentials:api-secret");
+            client.SetApiCredentials(apikey, apisecret);
+
+            return new ListenKeyManager(loggerFactory, client, interval);
+        }
+
+        /// <summary>
+        /// Create an invalid ListenKeyManager (wrong credentials)
+        /// </summary>
+        /// <param name="interval">Autorenewal interval</param>
+        /// <returns>ListenKeyManager with incorrect credentials</returns>
+        private ListenKeyManager SetupInvalidCredentials(int interval)
+        {
+            var serviceProvider = ServiceProviderSingleton.Instance.ServiceProvider;
+            var loggerFactory = (ILoggerFactory)serviceProvider.GetService(typeof(ILoggerFactory));
+            
+            //Setup the clients
+            var client = new BinanceClient();
+
+            // Set credentials
+            string apikey = "myapikey";
+            string apisecret = "myapisecret";
+            client.SetApiCredentials(apikey, apisecret);
+
+            return new ListenKeyManager(loggerFactory, client, interval);
+        }
+    }
+}
