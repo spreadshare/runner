82896aa9befa8c6dbb774d3e16f975fb11d05fe3
HugoPeters1024 <hpeters1024@gmail.com>
2018-10-08 18:20:32 +0200

    Fixed warnings

--- SpreadShare.Tests/Adapters/TimerTests.cs
+++ SpreadShare.Tests/Adapters/TimerTests.cs
@@ -1,17 +1,28 @@
-using Xunit;
-using SpreadShare.Strategy;
-using Xunit.Abstractions;
 using System;
+using SpreadShare.Strategy;
 using SpreadShare.Tests;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace Tests.AdapterTests
 {
+    /// <summary>
+    /// Collection of tests for the TimerAdapter
+    /// </summary>
     public class TimerTests : BaseTest
     {
-        public TimerTests(ITestOutputHelper outputHelper) : base(outputHelper)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TimerTests"/> class.
+        /// </summary>
+        /// <param name="outputHelper">Output helper that writes to TestOutput</param>
+        public TimerTests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
         {
         }
 
+        /// <summary>
+        /// Tests if the timer executes the callback right away if the provided time is 0.
+        /// </summary>
         [Fact]
         public void TimerExecutesCallbackNoDelay()
         {
@@ -19,40 +30,51 @@ namespace Tests.AdapterTests
             var timer = new Timer(0, () => flag = true);
             long start = DateTimeOffset.Now.ToUnixTimeMilliseconds();
 
-            //Execute within 10 ms (be generous for the scheduler)
+            // Execute within 10 ms (be generous for the scheduler)
             System.Threading.Thread.Sleep(5);
             Assert.True(flag, "Callback was not called right away (+/- 5ms)");
         }
 
+        /// <summary>
+        /// Tests if the executes the callback after exactly one second.
+        /// </summary>
         [Fact]
         public void TimerExecutesAfterOneSecond()
         {
             bool flag = false;
             var timer = new Timer(1000, () => flag = true);
             long start = DateTimeOffset.Now.ToUnixTimeMilliseconds();
-            
-            while(GetPassedTime(start) < 995) { System.Threading.Thread.Sleep(1); }
-            if (flag) Assert.True(false, "Callback was called before one second (+/- 5ms)");
 
-            while(GetPassedTime(start) < 1005) { System.Threading.Thread.Sleep(1); }
+            while (GetPassedTime(start) < 995) { System.Threading.Thread.Sleep(1); }
+            if (flag) { Assert.True(false, "Callback was called before one second (+/- 5ms)"); }
+
+            while (GetPassedTime(start) < 1005) { System.Threading.Thread.Sleep(1); }
             Assert.True(flag, "Callback was not called after one second (+/- 5ms)");
         }
 
+        /// <summary>
+        /// Tests if the timer executes after a certain amount of milliseconds not divisible by 1000
+        /// </summary>
         [Fact]
         public void TimerExecutesWithPartialSeconds()
         {
             bool flag = false;
             var timer = new Timer(1337, () => flag = true);
             long start = DateTimeOffset.Now.ToUnixTimeMilliseconds();
-            
-            while(GetPassedTime(start) < 1337-5) { System.Threading.Thread.Sleep(1); }
-            if (flag) Assert.True(false, "Callback was called before 1500 (+/- 5) milliseconds");
-            
-            while(GetPassedTime(start) < 1337+5) { System.Threading.Thread.Sleep(1); }
+
+            while (GetPassedTime(start) < 1337 - 5) { System.Threading.Thread.Sleep(1); }
+            if (flag) { Assert.True(false, "Callback was called before 1500 (+/- 5) milliseconds"); }
+
+            while (GetPassedTime(start) < 1337 + 5) { System.Threading.Thread.Sleep(1); }
             Assert.True(flag, "Callback was not called after 1500 (+/- 5) milliseconds");
         }
 
-        private long GetPassedTime(long start)
+        /// <summary>
+        /// Helper function that calculates the passed number of milliseconds
+        /// </summary>
+        /// <param name="start">UnixTime from (in ms)</param>
+        /// <returns>Number of milliseconds since start</returns>
+        private static long GetPassedTime(long start)
         {
             return DateTimeOffset.Now.ToUnixTimeMilliseconds() - start;
         }
--- SpreadShare/Strategy/Timer.cs
+++ SpreadShare/Strategy/Timer.cs
@@ -5,59 +5,88 @@ using SpreadShare.Models;
 
 namespace SpreadShare.Strategy
 {
-   
-    internal class Timer  : IDisposable
+    /// <summary>
+    /// A wrapper for the System.Thread.Timer class that doesn't fail on long waiting times.
+    /// </summary>
+    internal class Timer : IDisposable
     {
         private readonly Action _callback;
         private readonly System.Threading.Timer _timer;
-        private uint _counter = 0;
         private readonly uint _targetCount;
         private readonly int _rest;
+        private uint _counter = 0;
         private bool _executed = false;
 
         /// <summary>
-        /// Constructor: Startes waiting period
+        /// Initializes a new instance of the <see cref="Timer"/> class.
+        /// The timer will be started right away.
         /// </summary>
         /// <param name="ms">Waiting time</param>
         /// <param name="callback">Callback to execute after wait; can't be null</param>
-        public Timer(uint ms, Action callback) {
+        public Timer(uint ms, Action callback)
+        {
             _callback = callback ?? throw new ArgumentException("Callback can't be null");
 
             _targetCount = ms / 1000;
             _rest = (int)(ms % 1000);
-            
-            
-            //As suggested by https://adrientorris.github.io/aspnet-core/how-to-implement-timer-netcoreapp1-0-netcoreapp1-1.html
+
+            // As suggested by https://adrientorris.github.io/aspnet-core/how-to-implement-timer-netcoreapp1-0-netcoreapp1-1.html
             var autoEvent = new AutoResetEvent(false);
-            _timer = new System.Threading.Timer(Execute, autoEvent, 0, 1000);      
+            _timer = new System.Threading.Timer(Execute, autoEvent, 0, 1000);
         }
 
+        /// <summary>
+        /// Makes sure the callback is never executed
+        /// </summary>
+        public void Stop()
+        {
+            _executed = true;
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Disposes the current object's resource
+        /// </summary>
+        /// <param name="disposing">Whether to dispose the resources of the object</param>
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _timer?.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Wrapper function for the callback method.
+        /// </summary>
+        /// <param name="stateInfo">Provided by the System EventHandler</param>
         private async void Execute(object stateInfo)
         {
-            if (_executed) return;
+            if (_executed)
+            {
+                return;
+            }
+
             if (_counter < _targetCount)
             {
                 Console.WriteLine($"Call #{_counter++}    {DateTime.UtcNow}");
                 return;
             }
 
-            if (_rest > 0) await Task.Delay(_rest);
-            
+            if (_rest > 0)
+            {
+                await Task.Delay(_rest);
+            }
+
             Console.WriteLine("Executing Callback");
             _executed = true;
             _callback();
-        }
-
-        
-        public ResponseObject Stop()
-        {
-            _executed = true;
-            return new ResponseObject(ResponseCodes.Success);
-        }
-
-        public void Dispose()
-        {
-            _timer?.Dispose();
         }
     }
 }
