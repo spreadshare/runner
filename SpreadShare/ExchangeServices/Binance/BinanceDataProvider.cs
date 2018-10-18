15c089cbc2cae0d8c12aace9e1c93ceff298e36f
Yorick van Zweeden <git@yorickvanzweeden.nl>
2018-10-18 16:53:58 +0200

    Add ExchangeFactoryService

---
+++ SpreadShare/ExchangeServices/Binance/BinanceDataProvider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using SpreadShare.Models;
+
+namespace SpreadShare.ExchangeServices.Binance
+{
+    /// <summary>
+    /// Provides data gathering capabilities for Binance.
+    /// </summary>
+    internal class BinanceDataProvider : IDataProvider
+    {
+        /// <inheritdoc />
+        public ResponseObject<decimal> GetCurrentPriceLastTrade(CurrencyPair pair)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public ResponseObject<decimal> GetCurrentPriceTopBid(CurrencyPair pair)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public ResponseObject<decimal> GetCurrentPriceTopAsk(CurrencyPair pair)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public ResponseObject<decimal> GetPerformancePastHours(CurrencyPair pair, double hoursBack, DateTime endTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public ResponseObject<Tuple<CurrencyPair, decimal>> GetTopPerformance(List<CurrencyPair> pairs, double hoursBack, DateTime endTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SpreadShare/ExchangeServices/Binance/BinanceTradingProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Binance.Net.Objects;
+using SpreadShare.Models;
+
+namespace SpreadShare.ExchangeServices.Binance
+{
+    /// <summary>
+    /// Provides trading capabilities for Binance.
+    /// </summary>
+    internal class BinanceTradingProvider : ITradingProvider
+    {
+        /// <inheritdoc />
+        public ResponseObject PlaceFullMarketOrder(CurrencyPair pair, OrderSide side)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public ResponseObject CancelOrder(long orderId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
