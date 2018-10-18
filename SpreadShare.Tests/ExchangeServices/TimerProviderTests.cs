6d92e14596008bbed8b5deaaf36a3acc17c01f73
HugoPeters1024 <hpeters1024@gmail.com>
2018-10-18 15:47:53 +0200

    Add unit tests

---
+++ SpreadShare.Tests/ExchangeServices/TimerProviderTests.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+using SpreadShare.ExchangeServices;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SpreadShare.Tests.ExchangeServices
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Tests regadring the timer provider.
+    /// </summary>
+    public class TimerProviderTests : BaseTest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TimerProviderTests"/> class.
+        /// </summary>
+        /// <param name="outputHelper">used to create output</param>
+        public TimerProviderTests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
+        {
+        }
+
+        /// <summary>
+        /// Test if the timer provides generates an argument exception if the given callback is null.
+        /// </summary>
+        [Fact]
+        public void NoCallbackThrows()
+        {
+            var t = new ExchangeTimerProvider();
+            Assert.Throws<ArgumentException>(() => t.SetTimer(0, null));
+        }
+    }
+}
