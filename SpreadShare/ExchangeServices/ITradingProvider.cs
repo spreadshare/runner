0e2faeb1d7ef2ee91029047b53fee7958e0b76a9
Yorick van Zweeden <git@yorickvanzweeden.nl>
2018-10-18 16:03:27 +0200

    Add DataProvider and TradingProvider

---
+++ SpreadShare/ExchangeServices/ITradingProvider.cs
@@ -0,0 +1,26 @@
+﻿using Binance.Net.Objects;
+using SpreadShare.Models;
+
+namespace SpreadShare.ExchangeServices
+{
+    /// <summary>
+    /// Interface for trading with an exchange
+    /// </summary>
+    internal interface ITradingProvider
+    {
+        /// <summary>
+        /// Places market order with the full amount of given pair
+        /// </summary>
+        /// <param name="pair">Currency pair to trade with</param>
+        /// <param name="side">Whether to buy or sell</param>
+        /// <returns>A response object indicating the status of the market order</returns>
+        ResponseObject PlaceFullMarketOrder(CurrencyPair pair, OrderSide side);
+
+        /// <summary>
+        /// Cancels order
+        /// </summary>
+        /// <param name="orderId">Id of the order</param>
+        /// <returns>A response object with the results of the action</returns>
+        ResponseObject CancelOrder(long orderId);
+    }
+}
