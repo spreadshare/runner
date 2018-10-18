a5a877cfd0378e68f7360d953b663cd2f4b569ff
Yorick van Zweeden <git@yorickvanzweeden.nl>
2018-10-18 14:28:50 +0200

    Move AllocationManager to separate folder

--- SpreadShare/ExchangeServices/Allocation/AllocationManager.cs
+++ SpreadShare/ExchangeServices/Allocation/AllocationManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
 using SpreadShare.Models;
 
-namespace SpreadShare.ExchangeServices
+namespace SpreadShare.ExchangeServices.Allocation
 {
     /// <summary>
     /// This class provides allocation management for multiple strategies.
