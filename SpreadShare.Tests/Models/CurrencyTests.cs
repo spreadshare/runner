443b12fe331bcd5f678bd355f63bc623c784f997
HugoPeters1024 <hpeters1024@gmail.com>
2018-10-21 15:50:31 +0200

    add documentation to tests

--- SpreadShare.Tests/Models/CurrencyTests.cs
+++ SpreadShare.Tests/Models/CurrencyTests.cs
@@ -5,30 +5,47 @@ using Xunit.Abstractions;
 
 namespace SpreadShare.Tests.Models
 {
+    /// <summary>
+    /// Tests regarding currencies
+    /// </summary>
     public class CurrencyTests : BaseTest
     {
-        public CurrencyTests(ITestOutputHelper outputHelper) : base(outputHelper)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CurrencyTests"/> class.
+        /// </summary>
+        /// <param name="outputHelper">Used to create output</param>
+        public CurrencyTests(ITestOutputHelper outputHelper)
+            : base(outputHelper)
         {
         }
 
+        /// <summary>
+        /// Check if the same currencies are in fact equal
+        /// </summary>
         [Fact]
         public void SameCurrencyEquality()
         {
             var a = new Currency("ETH");
             var b = new Currency("ETH");
-            
+
             Assert.True(a == b, $"Same currencies where not evaluated as equal");
         }
 
+        /// <summary>
+        /// Check if different currencies are in fact unequal
+        /// </summary>
         [Fact]
         public void DifferentCurrencyInequality()
         {
             var a = new Currency("BNB");
             var b = new Currency("VTHO");
-            
+
             Assert.False(a == b, "Different currencies where evaluated as equal");
         }
 
+        /// <summary>
+        /// Check if trying to create a currency with symbol 'null' throws.
+        /// </summary>
         [Fact]
         public void NullCurrencyThrows()
         {
