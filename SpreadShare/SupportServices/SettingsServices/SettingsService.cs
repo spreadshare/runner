b206f4649ed585d13f5953e50bb2345ec0ca60bf
Yorick van Zweeden <git@yorickvanzweeden.nl>
2018-10-22 21:24:47 +0200

    Read allocation from configuration

--- SpreadShare/SupportServices/SettingsServices/SettingsService.cs
+++ SpreadShare/SupportServices/SettingsServices/SettingsService.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Reflection;
 using System.Text.RegularExpressions;
 using Binance.Net;
 using Binance.Net.Objects;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using SpreadShare.Algorithms;
 using SpreadShare.ExchangeServices;
 using SpreadShare.Models;
 
@@ -33,6 +37,11 @@ namespace SpreadShare.SupportServices.SettingsServices
         /// <summary>
         /// Gets the binance settings
         /// </summary>
+        public Dictionary<Exchange, Dictionary<Type, decimal>> AllocationSettings { get; private set; }
+
+        /// <summary>
+        /// Gets the binance settings
+        /// </summary>
         public BinanceSettings BinanceSettings { get; private set; }
 
         /// <summary>
@@ -51,6 +60,7 @@ namespace SpreadShare.SupportServices.SettingsServices
             try
             {
                 // Enables parsing functionality for currencies and should be called first.
+                ParseAllocationSettings();
                 DownloadCurrencies();
                 ParseSimpleBandwagonSettings();
                 BinanceSettings = _configuration.GetSection("BinanceClientSettings").Get<BinanceSettings>();
@@ -146,6 +156,100 @@ namespace SpreadShare.SupportServices.SettingsServices
             // Parse exchange to enum Exchange
             var exchange = _configuration.GetSection("SimpleBandWagonAlgorithm:Exchange").Get<string>();
             SimpleBandWagonAlgorithmSettings.Exchange = Enum.Parse<Exchange>(exchange);
+        }
+
+        /// <summary>
+        /// Parse allocations settings by evaluating each child as algorithm.
+        /// Note that duplicate entries are automatically removed by IConfiguration.
+        /// Only the last entry remains.
+        /// </summary>
+        private void ParseAllocationSettings()
+        {
+            AllocationSettings = new Dictionary<Exchange, Dictionary<Type, decimal>>();
+
+            // Get configuration section
+            var allocations = _configuration.GetSection("AllocationSettings").GetChildren();
+            if (!allocations.Any())
+            {
+                throw new Exception("Could not find segment AllocationSettings");
+            }
+
+            // Iterate through assembly and classes and retrieve a dictionary with the solution's classes
+            var classes = GetClasses();
+
+            // Iterate over exchanges
+            foreach (var exchange in allocations)
+            {
+                // Enum not found --> Exception
+                var exchangeEnum = Enum.Parse<Exchange>(exchange.Key);
+                AllocationSettings.Add(exchangeEnum, new Dictionary<Type, decimal>());
+
+                // Iterate over algorithms
+                foreach (var algorithm in exchange.GetChildren())
+                {
+                    Type algorithmType = classes[algorithm.Key].AsType();
+
+                    if (algorithmType == null)
+                    {
+                        throw new InvalidConstraintException($"Could not parse algorithm {algorithm.Key}");
+                    }
+
+                    if (algorithmType.BaseType != typeof(BaseAlgorithm))
+                    {
+                        throw new InvalidConstraintException($"The type {algorithm} does not implement BaseAlgorithm");
+                    }
+
+                    var allocation = decimal.Parse(algorithm.Value, NumberStyles.AllowDecimalPoint, new NumberFormatInfo());
+                    AllocationSettings[exchangeEnum].Add(algorithmType, allocation);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get a dictionary of classes with class name
+        /// </summary>
+        /// <returns>Dictionary of classes with class name</returns>
+        private Dictionary<string, TypeInfo> GetClasses()
+        {
+            // Get current assembly
+            Assembly thisAssembly = Array.Find(AppDomain.CurrentDomain.GetAssemblies(), assembly
+                => assembly.ManifestModule.Name.Contains("SpreadShare.dll", StringComparison.InvariantCulture));
+
+            // Check if the assembly was found
+            if (thisAssembly == null)
+            {
+                throw new InvalidProgramException("Could not find SpreadShare.dll");
+            }
+
+            // Get all defined classes
+            var typeInfos = thisAssembly.DefinedTypes;
+            Dictionary<string, TypeInfo> classes = new Dictionary<string, TypeInfo>();
+            foreach (var typeInfo in typeInfos)
+            {
+                var typeInfoName = typeInfo.Name;
+
+                // Remove noise
+                string[] patterns = { "`1", "+<>c" };
+                foreach (string pattern in patterns)
+                {
+                    if (typeInfoName.EndsWith(pattern, true, CultureInfo.InvariantCulture))
+                    {
+                        typeInfoName = typeInfoName.Substring(0, typeInfoName.Length - pattern.Length);
+                    }
+                }
+
+                if (!classes.ContainsKey(typeInfoName))
+                {
+                    classes.Add(typeInfoName, typeInfo);
+                }
+                else
+                {
+                    // Make sure the algorithm class does not have different parameterized types
+                    classes[typeInfoName] = null;
+                }
+            }
+
+            return classes;
         }
     }
 }
