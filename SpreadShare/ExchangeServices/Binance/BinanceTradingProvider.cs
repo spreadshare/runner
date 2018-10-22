629b73d834d7321b3ec4888a3d70e177eab3a6c0
HugoPeters1024 <hpeters1024@gmail.com>
2018-10-22 12:18:42 +0200

    Add market order for binance

--- SpreadShare/ExchangeServices/Binance/BinanceTradingProvider.cs
+++ SpreadShare/ExchangeServices/Binance/BinanceTradingProvider.cs
@@ -28,7 +28,21 @@ namespace SpreadShare.ExchangeServices.Binance
         /// <inheritdoc />
         public override ResponseObject PlaceFullMarketOrder(CurrencyPair pair, OrderSide side, decimal amount)
         {
-            throw new NotImplementedException();
+            var client = _communications.Client;
+            uint retries = 0;
+
+            while (retries++ < 5)
+            {
+                var query = client.PlaceOrder(pair.ToString(), side, OrderType.Market, amount);
+                if (query.Success)
+                {
+                    return new ResponseObject(ResponseCode.Success);
+                }
+                Logger.LogWarning(query.ToString());
+                Logger.LogWarning($"Placing market order ({side} {amount}{pair}) failed, retrying {retries}/5");
+            }
+            Logger.LogError($"Placing market order {side} {amount}{pair} failed after retrying 5 times");
+            return new ResponseObject(ResponseCode.Error);
         }
 
         /// <inheritdoc />
