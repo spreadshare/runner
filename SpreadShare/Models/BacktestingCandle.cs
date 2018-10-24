9748aff4e6000802579ed7982aba31d22557a0ee
Yorick van Zweeden <git@yorickvanzweeden.nl>
2018-10-24 15:52:04 +0200

    Add backtesting candle

---
+++ SpreadShare/Models/BacktestingCandle.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpreadShare.Models
+{
+    /// <summary>
+    /// Represents 1-minute candles for backtesting purposes.
+    /// </summary>
+    public class BacktestingCandle
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BacktestingCandle"/> class.
+        /// </summary>
+        /// <param name="timestamp">Timestamp of the candle</param>
+        /// <param name="open">Opening price of candle</param>
+        /// <param name="close">Closing price of candle</param>
+        /// <param name="high">Highest price during time period</param>
+        /// <param name="low">Lowest price during time period</param>
+        /// <param name="volume">Volume in the time period</param>
+        /// <param name="tradingPair">Tradingpair of the candle</param>
+        public BacktestingCandle(uint timestamp, decimal open, decimal close, decimal high, decimal low, decimal volume, string tradingPair)
+        {
+            Timestamp = timestamp;
+            Open = open;
+            Close = close;
+            High = high;
+            Low = low;
+            Volume = volume;
+            TradingPair = tradingPair;
+        }
+
+        /// <summary>
+        /// Gets or sets the timestamp of the candle.
+        /// </summary>
+        [Key]
+        public uint Timestamp { get; set; }
+
+        /// <summary>
+        /// Gets or sets the opening price of the candle.
+        /// </summary>
+        public decimal Open { get; set; }
+
+        /// <summary>
+        /// Gets or sets the closing price of the candle.
+        /// </summary>
+        public decimal Close { get; set; }
+
+        /// <summary>
+        /// Gets or sets the highest price of the candle.
+        /// </summary>
+        public decimal High { get; set; }
+
+        /// <summary>
+        /// Gets or sets the lowest price of the candle.
+        /// </summary>
+        public decimal Low { get; set; }
+
+        /// <summary>
+        /// Gets or sets the volume of the candle.
+        /// </summary>
+        public decimal Volume { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tradingpair.
+        /// </summary>
+        public string TradingPair { get; set; }
+    }
+}
