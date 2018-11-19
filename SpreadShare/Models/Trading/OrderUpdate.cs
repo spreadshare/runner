905ba12aacd3da92d047bcbbcfb75a29de0c947f
HugoPeters1024 <hpeters1024@gmail.com>
2018-11-19 12:19:37 +0100

    Add order type enum

--- SpreadShare/Models/Trading/OrderUpdate.cs
+++ SpreadShare/Models/Trading/OrderUpdate.cs
@@ -8,24 +8,30 @@
         /// <summary>
         /// Initializes a new instance of the <see cref="OrderUpdate"/> class.
         /// </summary>
+        /// <param name="orderType">The type of the order</param>
+        /// <param name="createdTimeStamp">The timestamp at which the order is created</param>
         /// <param name="setPrice">Price at which the order was set</param>
         /// <param name="side">Side of the order</param>
         /// <param name="pair">The pair of order</param>
         /// <param name="amount">The quantity of the order</param>
         /// <param name="orderId">The unique id of the order</param>
         public OrderUpdate(
+            long orderId,
+            OrderTypes orderType,
+            long createdTimeStamp,
             decimal setPrice,
             OrderSide side,
             TradingPair pair,
-            decimal amount,
-            long orderId)
+            decimal amount)
         {
+            OrderId = orderId;
+            OrderType = orderType;
+            CreatedTimeStamp = createdTimeStamp;
             SetPrice = setPrice;
             Side = side;
             Status = OrderStatus.New;
             Pair = pair;
             Amount = amount;
-            OrderId = orderId;
             SetPrice = setPrice;
             TotalFilled = 0;
             LastFillIncrement = 0;
@@ -74,9 +80,66 @@
         }
 
         /// <summary>
+        /// Type of the order
+        /// </summary>
+        public enum OrderTypes
+        {
+            /// <summary>
+            /// A market order
+            /// </summary>
+            Market,
+
+            /// <summary>
+            /// A limit order
+            /// </summary>
+            Limit,
+            
+            /// <summary>
+            /// A stoploss order
+            /// </summary>
+            StopLoss,
+            
+            /// <summary>
+            /// A stoploss limit order
+            /// </summary>
+            StopLossLimit,
+            
+            /// <summary>
+            /// A take profit order
+            /// </summary>
+            TakeProfit,
+            
+            /// <summary>
+            /// A take profit limit order
+            /// </summary>
+            TakeProfitLimit,
+            
+            /// <summary>
+            /// A limit maker order
+            /// </summary>
+            LimitMaker
+            
+        }
+
+        /// <summary>
         /// Gets the unique id of the order
         /// </summary>
         public long OrderId { get; }
+        
+        /// <summary>
+        /// Gets the type fo the order
+        /// </summary>
+        public OrderTypes OrderType { get; }
+        
+        /// <summary>
+        /// Gets the timestamp at which the order was created
+        /// </summary>
+        public long CreatedTimeStamp { get; }
+        
+        /// <summary>
+        /// Gets the timestamp at which the order was filled
+        /// </summary>
+        public long FilledTimeStamp { get; set; }
 
         /// <summary>
         /// Gets the price at which the order was set.
