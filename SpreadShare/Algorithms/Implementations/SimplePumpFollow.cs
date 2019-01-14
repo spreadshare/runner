1beae138da52b39bb3ff8d5810105a12ad1b4581
Raoul Schipper <mail@raoulschipper.com>
2019-01-14 10:59:43 +0100

    Fix commenting on SimplePumpFollow

--- SpreadShare/Algorithms/Implementations/SimplePumpFollow.cs
+++ SpreadShare/Algorithms/Implementations/SimplePumpFollow.cs
@@ -20,7 +20,7 @@ namespace SpreadShare.Algorithms.Implementations
         /// <inheritdoc />
         protected override EntryState<SimplePumpFollowSettings> Initial => new WelcomeState();
 
-         // Buy when the price dips more than X percent in Y minutes, and sell after Z% recovery or after A hours
+         // Buy when the price rises more than X percent in Y minutes, and sell after Z% followthrough or after A hours
          private class WelcomeState : EntryState<SimplePumpFollowSettings>
          {
              public override State<SimplePumpFollowSettings> OnTimerElapsed()
@@ -131,27 +131,27 @@ namespace SpreadShare.Algorithms.Implementations
     internal class SimplePumpFollowSettings : AlgorithmSettings
     {
         /// <summary>
-        /// Gets or sets how much something needs to fall to be considered a dip
+        /// Gets or sets how much something needs to rise in a short time to be considered a pump
         /// </summary>
         public decimal FirstCheck { get; set; }
 
         /// <summary>
-        /// Gets or sets The diptime, how quickly the dip needs to happen to be considered a dip
+        /// Gets or sets The SecondCheck, how much something needs to rise to be considered a followthrough
         /// </summary>
         public decimal SecondCheck { get; set; }
 
         /// <summary>
-        /// Gets or sets recovery, determines how much profit the system should try to get before selling
+        /// Gets or sets profittake, determines where our exit should be
         /// </summary>
         public decimal ProfitTake { get; set; }
 
         /// <summary>
-        /// Gets or sets Stoptime, determines how long to wait untill we get out and try again
+        /// Gets or sets Stoptime, determines how long to wait untill we get out no matter the PnL
         /// </summary>
         public int StopTime { get; set; }
 
         /// <summary>
-        /// Gets or sets Stoptime, determines how long to wait untill we get out and try again
+        /// Gets or sets StopPrice, determines how low our stop should be placed
         /// </summary>
         public decimal StopPrice { get; set; }
     }
