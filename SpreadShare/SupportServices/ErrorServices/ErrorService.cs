26ebd5a4c4daedd8219a0d1ed75a6aba696649ba
HugoPeters1024 <hpeters1024@gmail.com>
2019-01-16 10:41:18 +0100

    Remove unused imports: ERORRRORR!!

--- SpreadShare/SupportServices/ErrorServices/ErrorService.cs
+++ SpreadShare/SupportServices/ErrorServices/ErrorService.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Diagnostics;
 using System.Net.Mail;
-using System.Reflection.Emit;
 using System.Text;
 using Microsoft.Extensions.Logging;
 using SpreadShare.Algorithms;
