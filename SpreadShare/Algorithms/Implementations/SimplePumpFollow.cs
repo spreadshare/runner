cf7dafca1250c6267549bbcd7993880b3a567c00
Raoul Schipper <mail@raoulschipper.com>
2019-01-28 11:34:46 +0100

    Fix TimedShortDip and small errors

--- SpreadShare/Algorithms/Implementations/SimplePumpFollow.cs
+++ SpreadShare/Algorithms/Implementations/SimplePumpFollow.cs
@@ -10,14 +10,14 @@ namespace SpreadShare.Algorithms.Implementations
 {
     /// <summary>
     /// The first pumpfollowing algorithm.
-    /// buys when the market has an large pump, and sells into an expected future pump
+    /// buys when the market has an large pump, and sells into an expected future pump.
     /// </summary>
     internal class SimplePumpFollow : BaseAlgorithm<SimplePumpFollowSettings>
     {
-        /// <inheritdoc />
-        protected override EntryState<SimplePumpFollowSettings> Initial => new WelcomeState();
+         /// <inheritdoc />
+         protected override EntryState<SimplePumpFollowSettings> Initial => new WelcomeState();
 
-         // Buy when the price rises more than X percent in Y minutes, and sell after Z% followthrough or after A hours
+         // Buy when the price rises more than X percent in Y minutes, and sell after Z% followthrough or after A hours.
          private class WelcomeState : EntryState<SimplePumpFollowSettings>
          {
              public override State<SimplePumpFollowSettings> OnTimerElapsed()
@@ -37,25 +37,25 @@ namespace SpreadShare.Algorithms.Implementations
              {
                  bool longPerformance = data.GetPerformancePastHours(
                                             AlgorithmSettings.ActiveTradingPairs.First(),
-                             8).Data > (1 + AlgorithmSettings.FirstCheck);
+                                            8) > (1 + AlgorithmSettings.FirstCheck);
                  bool shortPerformance = data.GetPerformancePastHours(
                                             AlgorithmSettings.ActiveTradingPairs.First(),
                                             3) < (1 - AlgorithmSettings.SecondCheck);
-                if (longPerformance && shortPerformance)
-                {
+                 if (longPerformance && shortPerformance)
+                 {
                     return new BuyState();
-                }
+                 }
 
-                return new NothingState<SimplePumpFollowSettings>();
-            }
+                 return new NothingState<SimplePumpFollowSettings>();
+             }
 
-            protected override void Run(TradingProvider trading, DataProvider data)
-            {
-            }
-        }
+             protected override void Run(TradingProvider trading, DataProvider data)
+             {
+             }
+         }
 
-        private class BuyState : State<SimplePumpFollowSettings>
-        {
+         private class BuyState : State<SimplePumpFollowSettings>
+         {
             private OrderUpdate _limitsell;
             private bool _stophit;
 
@@ -97,10 +97,10 @@ namespace SpreadShare.Algorithms.Implementations
 
                 _stophit = currentPrice < sellPrice;
             }
-        }
+         }
 
-        private class StopState : State<SimplePumpFollowSettings>
-        {
+         private class StopState : State<SimplePumpFollowSettings>
+         {
             private OrderUpdate oldlimit;
 
             public StopState(OrderUpdate limitsell)
@@ -119,7 +119,7 @@ namespace SpreadShare.Algorithms.Implementations
                 trading.ExecuteFullMarketOrderSell(AlgorithmSettings.ActiveTradingPairs.First());
                 SetTimer(TimeSpan.Zero);
             }
-        }
+         }
     }
 
     /// <summary>
@@ -128,27 +128,27 @@ namespace SpreadShare.Algorithms.Implementations
     internal class SimplePumpFollowSettings : AlgorithmSettings
     {
         /// <summary>
-        /// Gets or sets how much something needs to rise in a short time to be considered a pump
+        /// Gets or sets how much something needs to rise in a short time to be considered a pump.
         /// </summary>
         public decimal FirstCheck { get; set; }
 
         /// <summary>
-        /// Gets or sets The SecondCheck, how much something needs to rise to be considered a followthrough
+        /// Gets or sets The SecondCheck, how much something needs to rise to be considered a followthrough.
         /// </summary>
         public decimal SecondCheck { get; set; }
 
         /// <summary>
-        /// Gets or sets profittake, determines where our exit should be
+        /// Gets or sets profittake, determines where our exit should be.
         /// </summary>
         public decimal ProfitTake { get; set; }
 
         /// <summary>
-        /// Gets or sets Stoptime, determines how long to wait untill we get out no matter the PnL
+        /// Gets or sets Stoptime, determines how long to wait untill we get out no matter the PnL.
         /// </summary>
         public int StopTime { get; set; }
 
         /// <summary>
-        /// Gets or sets StopPrice, determines how low our stop should be placed
+        /// Gets or sets StopPrice, determines how low our stop should be placed.
         /// </summary>
         public decimal StopPrice { get; set; }
     }
--- SpreadShare/Algorithms/Implementations/TimedShortDip.cs
+++ SpreadShare/Algorithms/Implementations/TimedShortDip.cs
@@ -13,14 +13,14 @@ namespace SpreadShare.Algorithms.Implementations
 {
     /// <summary>
     /// The first short dip algorithm.
-    /// buys when the market has an unesecary dip, and sell after a set amount of time
+    /// buys when the market has an unesecary dip, and sell after a set amount of time.
     /// </summary>
     internal class TimedShortDip : BaseAlgorithm<TimedShortDipSettings>
     {
-        /// <inheritdoc />
-        protected override EntryState<TimedShortDipSettings> Initial => new WelcomeState();
+         /// <inheritdoc />
+         protected override EntryState<TimedShortDipSettings> Initial => new WelcomeState();
 
-         // Buy when the price dips more than X percent in Y minutes, and sell after Z% recovery or after A hours
+         // Buy when the price dips more than X percent in Y minutes, and sell after Z% recovery or after A hours.
          private class WelcomeState : EntryState<TimedShortDipSettings>
          {
              public override State<TimedShortDipSettings> OnTimerElapsed()
@@ -40,7 +40,7 @@ namespace SpreadShare.Algorithms.Implementations
              {
                  bool performance = data.GetPerformancePastHours(
                                         AlgorithmSettings.ActiveTradingPairs.First(),
-                             AlgorithmSettings.DipTime).Data < (1 - AlgorithmSettings.DipPercent);
+                                        AlgorithmSettings.DipTime) < (1 - AlgorithmSettings.DipPercent);
                  if (performance)
                  {
                      return new BuyState();
@@ -65,7 +65,7 @@ namespace SpreadShare.Algorithms.Implementations
 
              protected override void Run(TradingProvider trading, DataProvider data)
              {
-                 buyorder = trading.PlaceFullMarketOrderBuy(AlgorithmSettings.ActiveTradingPairs.First()).Data;
+                 buyorder = trading.ExecuteFullMarketOrderBuy(AlgorithmSettings.ActiveTradingPairs.First());
                  SetTimer(TimeSpan.FromHours(AlgorithmSettings.ExitTime));
              }
          }
@@ -86,31 +86,31 @@ namespace SpreadShare.Algorithms.Implementations
 
              protected override void Run(TradingProvider trading, DataProvider data)
              {
-                 trading.PlaceFullMarketOrderSell(oldbuy.Pair);
+                 trading.ExecuteFullMarketOrderSell(oldbuy.Pair);
                  SetTimer(TimeSpan.Zero);
              }
          }
      }
 
     /// <summary>
-    /// The TimedShortDip settings
+    /// The TimedShortDip settings.
     /// </summary>
     internal class TimedShortDipSettings : AlgorithmSettings
     {
         /// <summary>
-        /// Gets or sets how much something needs to fall to be considered a dip, a 3% dip is given as 0.03
+        /// Gets or sets how much something needs to fall to be considered a dip, a 3% dip is given as 0.03.
         /// </summary>
         public decimal DipPercent { get; set; }
 
         /// <summary>
-        /// Gets or sets The diptime, how quickly the dip needs to happen to be considered a dip in hours
+        /// Gets or sets The diptime, how quickly the dip needs to happen to be considered a dip in hours.
         /// </summary>
         public double DipTime { get; set; }
 
         /// <summary>
-        /// Gets or sets Stoptime, determines how long to wait untill we get out and try again in hours
+        /// Gets or sets Stoptime, determines how long to wait untill we get out and try again in hours.
         /// </summary>
-        public int ExitTime { get; set; }
+        public double ExitTime { get; set; }
     }
 }
 
