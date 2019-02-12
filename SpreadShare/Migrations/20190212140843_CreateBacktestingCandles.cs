d76a5700594937da79bab730e425f84a60b14041
Yorick van Zweeden <git@yorickvanzweeden.nl>
2019-02-12 15:16:03 +0100

    Set composite FK on BacktestingCandle

--- SpreadShare/Migrations/20190212140843_CreateBacktestingCandles.cs
+++ SpreadShare/Migrations/20190212140843_CreateBacktestingCandles.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #pragma warning disable
 
@@ -13,16 +13,16 @@ namespace SpreadShare.Migrations
                 columns: table => new
                 {
                     Timestamp = table.Column<long>(nullable: false),
+                    TradingPair = table.Column<string>(nullable: false),
                     Open = table.Column<decimal>(nullable: false),
                     Close = table.Column<decimal>(nullable: false),
                     High = table.Column<decimal>(nullable: false),
                     Low = table.Column<decimal>(nullable: false),
-                    Volume = table.Column<decimal>(nullable: false),
-                    TradingPair = table.Column<string>(nullable: true)
+                    Volume = table.Column<decimal>(nullable: false)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Candles", x => x.Timestamp);
+                    table.PrimaryKey("PK_Candles", x => new { x.Timestamp, x.TradingPair });
                 });
         }
 
