975d9df79d63a132e3d3dc1334d0fca578b76243
HugoPeters1024 <hpeters1024@gmail.com>
2019-03-03 14:33:23 +0100

    Implement exponential backoff + tests

--- SpreadShare/Utilities/HelperMethods.cs
+++ SpreadShare/Utilities/HelperMethods.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Threading;
+using Dawn;
 using Microsoft.Extensions.Logging;
 using SpreadShare.Models;
 
@@ -10,15 +12,20 @@ namespace SpreadShare.Utilities
     internal static class HelperMethods
     {
         /// <summary>
-        /// Retry a ReponseObject method a number of times.
+        /// Retry a ResponseObject method a number of times.
         /// </summary>
         /// <param name="method">The method to retry.</param>
         /// <param name="logger">Logger to report to.</param>
         /// <param name="maxRetries">Maximum number of retries (default 5).</param>
+        /// <param name="backoffMillis">The ground number for the exponential backoff delay.</param>
         /// <typeparam name="T">Type of the result.</typeparam>
         /// <returns>First (if any) success response of <see pref="method"/>.</returns>
-        public static ResponseObject<T> RetryMethod<T>(Func<ResponseObject<T>> method, ILogger logger, int maxRetries = 5)
+        public static ResponseObject<T> RetryMethod<T>(Func<ResponseObject<T>> method, ILogger logger, int maxRetries = 5, int backoffMillis = 200)
         {
+            Guard.Argument(method).NotNull();
+            Guard.Argument(maxRetries).NotZero().NotNegative();
+            Guard.Argument(backoffMillis).NotNegative();
+
             for (int i = 0; i < maxRetries; i++)
             {
                 var result = method();
@@ -27,7 +34,10 @@ namespace SpreadShare.Utilities
                     return result;
                 }
 
-                logger.LogWarning($"{result.Message} - attempt {i + 1}/{maxRetries} for method {method.Method.Name}");
+                var backoff = TimeSpan.FromMilliseconds(backoffMillis * Math.Pow(2, i + 1));
+                Thread.Sleep((int)backoff.TotalMilliseconds);
+
+                logger?.LogWarning($"{result.Message} - attempt {i + 1}/{maxRetries} for method {method.Method.Name}");
             }
 
             return new ResponseObject<T>(ResponseCode.Error);
@@ -39,10 +49,11 @@ namespace SpreadShare.Utilities
         /// <param name="method">The method to retry.</param>
         /// <param name="logger">Logger to report to.</param>
         /// <param name="maxRetries">Maximum number of retries (default 5).</param>
+        /// <param name="backoffMillis">The ground number for the exponential backoff delay.</param>
         /// <returns>First (if any) success response of the <see pref="method"/>.</returns>
-        public static ResponseObject RetryMethod(Func<ResponseObject> method, ILogger logger, int maxRetries = 5)
+        public static ResponseObject RetryMethod(Func<ResponseObject> method, ILogger logger, int maxRetries = 5, int backoffMillis = 200)
         {
-            var query = RetryMethod<string>(method, logger, maxRetries);
+            var query = RetryMethod<string>(method, logger, maxRetries, backoffMillis);
             return new ResponseObject(query.Code);
         }
 
