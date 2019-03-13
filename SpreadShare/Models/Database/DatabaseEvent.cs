2e532068df30ff24416203fcad7f6b535fee9854
HugoPeters1024 <hpeters1024@gmail.com>
2019-03-13 11:02:42 +0100

    Remove unused properties from DatabaseEvent

--- SpreadShare/Models/Database/DatabaseEvent.cs
+++ SpreadShare/Models/Database/DatabaseEvent.cs
@@ -9,15 +9,5 @@ namespace SpreadShare.Models.Database
         /// Gets or sets the session that this event is related to.
         /// </summary>
         public AlgorithmSession Session { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether this session is active.
-        /// </summary>
-        public bool Active { get; set; }
-
-        /// <summary>
-        /// Gets or sets the name of the session.
-        /// </summary>
-        public string Name { get; set; }
     }
 }
