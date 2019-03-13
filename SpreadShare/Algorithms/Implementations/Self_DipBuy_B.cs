61a5407b304faba6207e346332c7b3bd9ea04a7a
Raoul Schipper <mail@raoulschipper.com>
2019-03-13 14:13:50 +0100

    Change and fix Dip buying algorithm

--- SpreadShare/Algorithms/Implementations/Self_DipBuy_B.cs
+++ SpreadShare/Algorithms/Implementations/Self_DipBuy_B.cs
@@ -12,88 +12,77 @@ namespace SpreadShare.Algorithms.Implementations
     /// The first short dip algorithm.
     /// buys when the market has an unesecary dip, and sell after recovery.
     /// </summary>
-    internal class ShortDip : BaseAlgorithm<ShortDipConfiguration>
+    internal class Self_DipBuy_B : BaseAlgorithm<Self_DipBuy_BConfiguration>
     {
         /// <inheritdoc />
-        protected override EntryState<ShortDipConfiguration> Initial => new WelcomeState();
+        protected override EntryState<Self_DipBuy_BConfiguration> Initial => new WelcomeState();
 
         // Buy when the price dips more than X percent in Y minutes, and sell after Z% recovery or after A hours
-        private class WelcomeState : EntryState<ShortDipConfiguration>
+        private class WelcomeState : EntryState<Self_DipBuy_BConfiguration>
         {
-            protected override State<ShortDipConfiguration> Run(TradingProvider trading, DataProvider data)
+            protected override State<Self_DipBuy_BConfiguration> Run(TradingProvider trading, DataProvider data)
             {
                 return new EntryState();
             }
         }
 
-        private class EntryState : EntryState<ShortDipConfiguration>
+        private class EntryState : EntryState<Self_DipBuy_BConfiguration>
         {
-            public override State<ShortDipConfiguration> OnMarketCondition(DataProvider data)
+            public override State<Self_DipBuy_BConfiguration> OnMarketCondition(DataProvider data)
             {
-                bool performance = data.GetPerformancePastHours(
-                                       AlgorithmConfiguration.TradingPairs.First(),
-                                       AlgorithmConfiguration.DipTime) < (1 - AlgorithmConfiguration.DipPercent);
-                if (performance)
+                // request the close of the current candle,
+                // see if it is at least DipPercent below the close 2 candles ago
+                bool dip = data.GetCandles(FirstPair, 1).close
+                                       <
+                           // request close 2 candles ago
+                           (data.GetCandles(FirstPair, 1, 2).close * (1 - AlgorithmConfiguration.DipPercent));
+                if (dip)
                 {
                     return new BuyState();
                 }
 
-                return new NothingState<ShortDipConfiguration>();
+                return new NothingState<Self_DipBuy_BConfiguration>();
             }
         }
 
-        private class BuyState : State<ShortDipConfiguration>
+        private class BuyState : State<Self_DipBuy_BConfiguration>
         {
-            private OrderUpdate _limitsell;
-
-            public override State<ShortDipConfiguration> OnTimerElapsed()
+            protected override State<Self_DipBuy_BConfiguration> Run(TradingProvider trading, DataProvider data)
             {
-                return new CancelState(_limitsell);
-            }
-
-            public override State<ShortDipConfiguration> OnOrderUpdate(OrderUpdate order)
-            {
-                if (order.OrderId == _limitsell.OrderId && order.Status == OrderUpdate.OrderStatus.Filled)
-                {
-                    return new EntryState();
-                }
-
-                return new NothingState<ShortDipConfiguration>();
-            }
-
-            protected override State<ShortDipConfiguration> Run(TradingProvider trading, DataProvider data)
-            {
-                var buyorder = trading.ExecuteFullMarketOrderBuy(AlgorithmConfiguration.TradingPairs.First());
-                _limitsell = trading.PlaceFullLimitOrderSell(
-                    AlgorithmConfiguration.TradingPairs.First(),
-                    buyorder.AverageFilledPrice * AlgorithmConfiguration.Recovery);
-                SetTimer(TimeSpan.FromHours(AlgorithmConfiguration.StopTime));
-                return new NothingState<ShortDipConfiguration>();
+                trading.ExecutePartialMarketOrderBuy(FirstPair, 0.7M);
+                return new WaitState();
             }
         }
 
-        private class CancelState : State<ShortDipConfiguration>
+        private class WaitState : State<Self_DipBuy_BConfiguration>
         {
-            private OrderUpdate oldlimit;
-
-            public CancelState(OrderUpdate limitsell)
+            public override State<Self_DipBuy_BConfiguration> OnTimerElapsed()
             {
-                oldlimit = limitsell;
+                return new SellState();
             }
 
-            protected override State<ShortDipConfiguration> Run(TradingProvider trading, DataProvider data)
+            protected override State<Self_DipBuy_BConfiguration> Run(TradingProvider trading, DataProvider data)
             {
-                trading.CancelOrder(oldlimit);
-                trading.ExecuteFullMarketOrderSell(AlgorithmConfiguration.TradingPairs.First());
+                double WaitMinutes = AlgorithmConfiguration.RecoveryTime * (int) AlgorithmConfiguration.CandleWidth;
+                SetTimer(TimeSpan.FromMinutes(WaitMinutes));
+                return new NothingState<Self_DipBuy_BConfiguration>();
+            }
+        }
+        
+        private class SellState : State<Self_DipBuy_BConfiguration>
+        {
+            protected override State<Self_DipBuy_BConfiguration> Run(TradingProvider trading, DataProvider data)
+            {
+                trading.ExecuteFullMarketOrderSell(FirstPair);
                 return new EntryState();
             }
         }
     }
 
     /// <summary>
-    /// The ShortDip settings.
+    /// The Self_DipBuy_B settings.
     /// </summary>
-    internal class ShortDipConfiguration : AlgorithmConfiguration
+    internal class Self_DipBuy_BConfiguration : AlgorithmConfiguration
     {
         /// <summary>
         /// Gets or sets how much something needs to fall to be considered a dip.
@@ -101,19 +90,9 @@ namespace SpreadShare.Algorithms.Implementations
         public decimal DipPercent { get; set; }
 
         /// <summary>
-        /// Gets or sets The diptime, how quickly the dip needs to happen to be considered a dip.
-        /// </summary>
-        public double DipTime { get; set; }
-
-        /// <summary>
         /// Gets or sets recovery, determines how much profit the system should try to get before selling.
         /// </summary>
-        public decimal Recovery { get; set; }
-
-        /// <summary>
-        /// Gets or sets Stoptime, determines how long to wait untill we get out and try again.
-        /// </summary>
-        public int StopTime { get; set; }
+        public double RecoveryTime { get; set; }
     }
 }
 
