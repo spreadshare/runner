89870bcf679d24ac2c6a7265bdcb51bb5fd418d4
HugoPeters1024 <hpeters1024@gmail.com>
2019-03-25 23:21:01 +0100

    Add Portfolio field to database session

---
+++ SpreadShare/Migrations/20190325221956_SessionAllocationJson.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#pragma warning disable
+
+namespace SpreadShare.Migrations
+{
+    public partial class SessionAllocationJson : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AllocationJson",
+                table: "Sessions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AllocationJson",
+                table: "Sessions");
+        }
+    }
+}
+
+#pragma warning restore
