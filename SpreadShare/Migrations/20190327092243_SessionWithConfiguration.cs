d38bcc13a1402a98308bb11334c392cf503470f7
HugoPeters1024 <hpeters1024@gmail.com>
2019-03-27 10:32:52 +0100

    Add AlgorithmConfiguration field to Session model

---
+++ SpreadShare/Migrations/20190327092243_SessionWithConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#pragma warning disable
+
+namespace SpreadShare.Migrations
+{
+    public partial class SessionWithConfiguration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AlgorithmConfiguration",
+                table: "Sessions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AlgorithmConfiguration",
+                table: "Sessions");
+        }
+    }
+}
+
+#pragma warning restore
